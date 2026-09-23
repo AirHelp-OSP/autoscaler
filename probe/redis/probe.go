7d895000c19bb79b0ff54dc0aae708f8efdcb542
Krzysztof Wiatrzyk <krzysztof.wiatrzyk@letsgodevops.pl>
2026-09-23 10:02:03 +0200

    feat: support auth and tls in config file

--- probe/redis/probe.go
+++ probe/redis/probe.go
@@ -2,15 +2,56 @@ package redis
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"net"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 )
 
 type Config struct {
-	Hosts    []string `yaml:"hosts"`
+	Hosts    []Host   `yaml:"hosts"`
 	ListKeys []string `yaml:"list_keys"`
+}
+
+type Host struct {
+	Address  string `yaml:"address"`
+	Password string `yaml:"password"`
+	TLS      bool   `yaml:"tls"`
+	URL      string `yaml:"url"`
+}
+
+// Accepts the legacy plain "host:port" string as well as the object form.
+func (h *Host) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if err := unmarshal(&h.Address); err == nil {
+		return nil
+	}
+
+	type plain Host
+	return unmarshal((*plain)(h))
+}
+
+func (h Host) options() (*redis.Options, error) {
+	if h.URL != "" {
+		return redis.ParseURL(os.ExpandEnv(h.URL))
+	}
+
+	opts := &redis.Options{
+		Addr:     os.ExpandEnv(h.Address),
+		Password: os.ExpandEnv(h.Password),
+	}
+
+	if h.TLS {
+		host, _, err := net.SplitHostPort(opts.Addr)
+		if err != nil {
+			return nil, err
+		}
+		opts.TLSConfig = &tls.Config{ServerName: host}
+	}
+
+	return opts, nil
 }
 
 type Probe struct {
@@ -28,14 +69,25 @@ func New(config *Config) (*Probe, error) {
 	}
 
 	ringOpts := make(map[string]string)
+	hostOpts := make(map[string]*redis.Options)
 
-	for i, addr := range config.Hosts {
+	for i, host := range config.Hosts {
+		opts, err := host.options()
+		if err != nil {
+			return &Probe{}, err
+		}
+
 		key := fmt.Sprintf("host%d", i+1)
-		ringOpts[key] = addr
+		ringOpts[key] = opts.Addr
+		hostOpts[key] = opts
 	}
 
 	c := redis.NewRing(&redis.RingOptions{
 		Addrs: ringOpts,
+		// Ring-level Password/TLSConfig are shared by all shards, so per-host settings are applied here.
+		NewClient: func(name string, _ *redis.Options) *redis.Client {
+			return redis.NewClient(hostOpts[name])
+		},
 	})
 
 	err := c.ForEachShard(context.Background(), func(ctx context.Context, shard *redis.Client) error {
